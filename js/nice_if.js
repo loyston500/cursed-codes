663c449d41a8d3df3f9c331114614a7b04fd600d
NajeebFarhan <najeebfarhan6345@gmail.com>
2021-02-20 20:31:44 +0530

    added nice_if.js

---
+++ js/nice_if.js
@@ -0,0 +1,14 @@
+const NICE = 69;
+
+if(NICE < 420)
+    if(NICE > 42.0)
+        if(NICE === 33 + 36)
+            if(NICE === 100 - 31)
+                if(NICE === 34 + 35)
+                    if(NICE === 70 - 1)
+                        if(NICE !== 70)
+                            if(NICE == "6" + "9")
+                                if(NICE === 69)
+                                    if(NICE === NICE)
+                                        console.log("Nice!");
+                                
